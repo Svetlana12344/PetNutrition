a8c1bd90b0f8cb79bc82e4f9bddc8952dcecaedc
Svetlana12344 <svetlana-andreeva.svet@yandex.ru>
2026-01-11 17:48:19 +0500

    models.py

--- calculations/models.py
+++ calculations/models.py
@@ -3,7 +3,6 @@ from pets.models import Pet, Food
 
 
 class DietPlan(models.Model):
-    """План питания"""
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE, verbose_name="Питомец")
     daily_calories = models.FloatField(verbose_name="Суточная норма калорий")
     protein_need = models.FloatField(verbose_name="Потребность в белке (г)")
@@ -18,14 +17,8 @@ class DietPlan(models.Model):
 
 
 class FoodRecommendation(models.Model):
-    """Рекомендация по корму"""
     diet_plan = models.ForeignKey(DietPlan, on_delete=models.CASCADE)
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
     daily_amount = models.FloatField(verbose_name="Суточная норма (г)")
     meal_count = models.IntegerField(verbose_name="Количество кормлений", default=2)
     notes = models.TextField(verbose_name="Примечания", blank=True)
-
-
-from django.db import models
-
-# Create your models here.
